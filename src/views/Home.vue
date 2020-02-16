2771d37e8fe4dc9832c36f5f31094bbfdbd4e8fd
Daniel Su <danielsu@protonmail.ch>
2020-02-15 23:01:05 -0800

    [+] update form data and form inputs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,40 @@
 <template>
   <div class="home-view-container">
     <h1>Adopt a new best friend!</h1>
-    {{ getAllCats.length }}
-    {{ animalsCount }}
     <button @click="togglePetForm" class="btn btn-primary">Add New Pet</button>
 
     <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
-      <b-form-group id="input-group-2" label="Pet's Name:" label-for="input-2">
+      <b-form-group id="input-group-1" label="Pet's Name:" label-for="input-1">
         <b-form-input
-          id="input-2"
+          id="input-1"
           v-model="formData.name"
           required
           placeholder="Enter name"
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-3" label="Species:" label-for="input-3">
+      <b-form-group id="input-group-2" label="Breed:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          type="string"
+          v-model="formData.breed"
+          required
+          placeholder="Enter breed"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Gender:" label-for="input-3">
         <b-form-select
           id="input-3"
-          v-model="formData.species"
-          :options="['cats', 'dogs']"
+          v-model="formData.gender"
+          :options="['Male', 'Female']"
           required
         ></b-form-select>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Pet's Age:" label-for="input-2">
+      <b-form-group id="input-group-4" label="Pet's Age:" label-for="input-4">
         <b-form-input
-          id="input-2"
+          id="input-4"
           type="number"
           v-model="formData.age"
           required
@@ -34,6 +42,53 @@
         ></b-form-input>
       </b-form-group>
 
+      <b-form-group id="input-group-5" label="Pet's Color:" label-for="input-5">
+        <b-form-input
+          id="input-5"
+          type="string"
+          v-model="formData.color"
+          required
+          placeholder="Enter color"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-6" label="Pet's Weight:" label-for="input-6">
+        <b-form-input
+          id="input-6"
+          type="number"
+          v-model="formData.weight"
+          required
+          placeholder="Enter weight"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-7" label="Pet's Location:" label-for="input-7">
+        <b-form-input
+          id="input-7"
+          type="string"
+          v-model="formData.location"
+          required
+          placeholder="Enter location"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-8" label="Notes:" label-for="input-8">
+        <b-form-input
+          id="input-8"
+          type="string"
+          v-model="formData.notes"
+          placeholder="Enter notes"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-9" label="Species:" label-for="input-9">
+        <b-form-select
+          id="input-9"
+          v-model="formData.species"
+          :options="['cats', 'dogs']"
+          required
+        ></b-form-select>
+      </b-form-group>
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
@@ -50,8 +105,14 @@ export default {
       showPetForm: false,
       formData: {
         name: '',
-        age: 0,
-        species: null
+        breed: '',
+        gender: '',
+        age: null,
+        color: '',
+        weight: null,
+        location: '',
+        notes: '',
+        species: ''
       }
     }
   },
