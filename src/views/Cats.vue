46a9e974e8ee4bcb37e6cd754e47802587871406
Daniel Su <danielsu@protonmail.ch>
2020-02-16 10:55:05 -0800

    [+] made table component modular

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,20 +1,21 @@
 <template>
   <div>
-    <h1>Cats for Adoption</h1>
-    <b-table striped hover :items="cats">
-        <template v-slot:cell(name)="data">
-          <router-link :to="`/pets/cats/${data.index}`">
-            {{ data.value }}
-          </router-link>
-      </template>
-    </b-table>
+    <PetTable
+      species="cats"
+      :pets="cats"
+    />
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import PetTable from '@/components/PetTable'
 
 export default {
+  name: 'Cats',
+  components: {
+    PetTable
+  },
   data () {
     return {}
   },
